c86192dce39408fad401ee0307eb349812af61be
Lucas Maupin <lucas.maupin@gmail.com>
2018-12-23 10:48:03 +0100

    Nu da__

---
+++ src/components/QuickSnakeCreator/quickSnakeCreator.scss.d.ts
@@ -0,0 +1,10 @@
+import { string } from "prop-types";
+
+interface IQuickSnakeCreatorScss {
+  wrapper: string;
+  input: string;
+  inputWrapper: string;
+}
+
+declare const styles: IQuickSnakeCreatorScss;
+export default styles;
---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Header } from "../../components/Header/Header";
+import { QuickSnakeCreator} from "../../components/quickSnakeCreator/QuickSnakeCreator";
+
+// State is never set so we use the '{}' type.
+export class LandingPage extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div id="LandingPageWrapper">
+        <Header/>
+        <QuickSnakeCreator/>
+        </div>
+    );
+  }
+}
