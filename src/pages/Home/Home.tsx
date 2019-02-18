7727a64ebdf26b9cc804084197ee080fbe6077d3
mattiasmucherie <mattias.mattias.m@gmail.com>
2019-02-19 00:07:47 +0100

    Remove an unused import

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import SnakeCard from "../../components/SnakeCard/SnakeCard";
 import PrivateSnakeCreator from "../../components/PrivateSnakeCreator/PrivateSnakeCreator";
