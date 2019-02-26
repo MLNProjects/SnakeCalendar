4d9c310b5dff7e28512ae2f4e337a946a9fc3946
mattiasmucherie <mattias.mattias.m@gmail.com>
2019-02-26 18:20:03 +0100

    Better support for mobile devices

--- src/components/SnakeCard/snakeCard.scss.d.ts
+++ src/components/SnakeCard/snakeCard.scss.d.ts
@@ -1,4 +1,3 @@
-export const snakeCard: string;
 export const card: string;
 export const cardTitle: string;
 export const cardDate: string;
--- src/pages/Home/home.scss.d.ts
+++ src/pages/Home/home.scss.d.ts
@@ -1,6 +1,2 @@
 export const homeWrapper: string;
 export const snakesWrapper: string;
-export const zoomable: string;
-export const zoomer: string;
-export const card: string;
-export const cardText: string;
