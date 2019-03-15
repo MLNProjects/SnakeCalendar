7296dfd05267646a935bf8c48d6f68510d18d49b
mattiasmucherie <mattias.mattias.m@gmail.com>
2019-03-15 22:57:42 +0100

    Add functions backend

---
+++ functions/src/index.ts
@@ -0,0 +1,23 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+export const onSnakeCreate = functions.database
+  .ref("/users/{userId}/snakes/{snakeId}")
+  .onCreate((snapshot: any, context) => {
+    const userId = context.params.userId;
+    const snakeId = context.params.snakeId;
+    console.log(`User ${userId} created snake ${snakeId}`);
+
+    const snakeData = snapshot.val();
+    const snakeName = addSnek(snakeData.snakeName);
+    return snapshot.ref.update({ snakeName: snakeName });
+  });
+
+function addSnek(text: string): string {
+  return text.replace(/\bsnek\b/g, "🐍");
+}
