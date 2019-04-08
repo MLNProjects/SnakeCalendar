6b6d07b7b22c1e2ef048d157524cc82ceb983087
mattiasmucherie <mattias.mattias.m@gmail.com>
2019-04-08 17:29:54 +0200

    Edit About page

--- src/pages/About/About.scss.d.ts
+++ src/pages/About/About.scss.d.ts
@@ -1,3 +1,4 @@
 export const aboutPageWrapper: string;
 export const headWrapper: string;
 export const contentWrapper: string;
+export const nameWrapper: string;
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,26 +1,62 @@
 import * as React from "react";
 import * as styles from "./About.scss";
+import { Link } from "react-router-dom";
 
 // State is never set so we use the '{}' type.
 class About extends React.Component<{}, {}> {
   public render() {
     return (
-      <div id={styles.aboutPageWrapper}>
+      <div className={styles.aboutPageWrapper}>
         <div className={styles.headWrapper}>
           <h1>About</h1>
           <h5>
-            This page will explain what was used in order to build this site.
+            SnakeCalender is an web application that helps you track your goals.
+            You give your snake the name of what you want to do and set how
+            often you want to do that. If you fail to do it, your snake dies and
+            you have to start over all over again.
           </h5>
+          <div className={styles.nameWrapper}>
+            <h6>
+              SnakeCalender was built and designed by{" "}
+              <a
+                href="https://www.linkedin.com/in/mattias-mucherie/"
+                target="_blank"
+              >
+                Mattias Mucherie
+              </a>
+              ,{" "}
+              <a
+                href="https://www.linkedin.com/in/lucas-maupin-b7299b150/"
+                target="_blank"
+              >
+                Lucas Maupin
+              </a>{" "}
+              ,{" "}
+              <a href="https://github.com/erl-j" target="_blank">
+                Nicolas Jonasson
+              </a>{" "}
+              and with a little help from{" "}
+              <a
+                href="https://www.linkedin.com/in/alexandre-blondot-a56a0ab5/"
+                target="_blank"
+              >
+                Alexandre Blondot
+              </a>
+              .
+            </h6>
+          </div>
         </div>
         <div className={styles.contentWrapper}>
           <p>
             The frontend of this project was built with:
-            <strong> TypeScript, React, Redux and materialise-css</strong>
+            <strong> TypeScript, React, Redux</strong> and{" "}
+            <strong>materialise-css</strong>.
           </p>
 
           <p>
             The backend of this project was built with:{" "}
-            <strong>Firebase and cron-jobs</strong>
+            <strong>Firebase, Firebase functions</strong> and{" "}
+            <strong>cron-jobs</strong>.
           </p>
         </div>
       </div>
